24c3315a4c1058864010fe8b8f02157a960729f9
jadenadams329 <jadenadams329@gmail.com>
2019-12-12 18:26:12 -0700

    fixing starwars

--- js/starwars.js
+++ js/starwars.js
@@ -1,4 +1,6 @@
 import { people } from '../assets/people.js'
+import { films } from '../assets/films.js';
+import { planets } from '../assets/planets.js';
 
 let mainArea = document.querySelector('main');
 
@@ -23,6 +25,31 @@ people.forEach((person) => {
 
     mainArea.appendChild(personDiv)
 });
+
+
+//films array
+films.forEach((film) => {
+    let filmDiv = document.createElement('div')
+    let title = document.createElement('h1')
+    let releaseDate = document.createElement('p')
+    let pic = document.createElement('img')
+
+    pic.setAttribute('class', 'picDivs')
+
+    title.textContent = film.title
+    releaseDate.textContent = film.release_date
+    pic.src = `https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.svg`
+
+    filmDiv.appendChild(title)
+    filmDiv.appendChild(releaseDate)
+    filmDiv.appendChild(pic)
+
+    mainArea.appendChild(filmDiv)
+ 
+
+})
+
+
 //grabs the character ID
 function getCharNumber(charURL) {
     let end = charURL.lastIndexOf('/')
@@ -33,6 +60,14 @@ function getCharNumber(charURL) {
         return charID
     }
 }
+
+function removeAnimationClasses() {
+    const buttons = userList.querySelectorAll("button");
+  
+    buttons.forEach(function(button) {
+      button.classList.remove = 'animated infinite bounce delay-2s;';
+    });
+  }
 
 const allDivs = Array.from(document.querySelectorAll('div'))
 
@@ -45,10 +80,11 @@ maleButton.addEventListener('click', () => {
         let matchedDiv = allDivs.find((oneDiv) => {
             return oneDiv.firstChild.textContent === character.name
         })
-        //matchedDiv.setAttribute("style", "display: none;")
-        matchedDiv.className = 'animated infinite bounce delay-2s'
+        matchedDiv.setAttribute("style", "display: none;")
+        
     })
 });
+
 //making female characters bounce on page
 let femaleButton = document.createElement('button')
 femaleButton.textContent = 'Female Characters'
@@ -57,10 +93,11 @@ femaleButton.addEventListener('click', () => {
         let matchedDiv = allDivs.find((oneDiv) => {
             return oneDiv.firstChild.textContent === character.name
         })
-        //matchedDiv.setAttribute("style", "display: none;")
-        matchedDiv.className = 'animated infinite bounce delay-2s;'
+        matchedDiv.setAttribute("style", "display: none;")
+        
     })
 });
+
 //making non gender characters bounce on page
 let otherButton = document.createElement('button')
 otherButton.textContent = 'Non-Gender Characters'
@@ -69,16 +106,46 @@ otherButton.addEventListener('click', () => {
         let matchedDiv = allDivs.find((oneDiv) => {
             return oneDiv.firstChild.textContent === character.name
         })
-        //matchedDiv.setAttribute("style", "display: none;")
-        matchedDiv.className = 'animated infinite bounce delay-2s;'
+        matchedDiv.setAttribute("style", "display: none;")
+        
     })
 });
+//Want to add films button
+let filmsButton = document.createElement('button')
+filmsButton.textContent = 'Films'
+filmsButton.addEventListener('click', () => {
+    otherCharacters.forEach(character => {
+        let matchedDiv = allDivs.find((oneDiv) => {
+            return oneDiv.firstChild.textContent === character.name
+        })
+        matchedDiv.setAttribute("style", "display: none;")
+        
+    })
+    femaleCharacters.forEach(character => {
+        let matchedDiv = allDivs.find((oneDiv) => {
+            return oneDiv.firstChild.textContent === character.name
+        })
+        matchedDiv.setAttribute("style", "display: none;")
+        
+    })
+    maleCharacters.forEach(character => {
+        let matchedDiv = allDivs.find((oneDiv) => {
+            return oneDiv.firstChild.textContent === character.name
+        })
+        matchedDiv.setAttribute("style", "display: none;")
+        
+    })
+
+});
+
 
 mainHeader.appendChild(maleButton)
 mainHeader.appendChild(femaleButton)
 mainHeader.appendChild(otherButton)
+mainHeader.appendChild(filmsButton)
 
 //filters
-const maleCharacters = people.filter(person => person.gender === 'male')
-const femaleCharacters = people.filter(person => person.gender === 'female')
+const maleCharacters = people.filter(person => person.gender !== 'male')
+const femaleCharacters = people.filter(person => person.gender !== 'female')
 const otherCharacters = people.filter(person => person.gender !== 'female' && person.gender !== 'male')
+
